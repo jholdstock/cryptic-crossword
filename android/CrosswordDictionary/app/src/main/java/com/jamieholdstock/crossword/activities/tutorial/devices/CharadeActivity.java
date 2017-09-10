f2d31a9d0d2ed344cc615886cda47b0b4662ecb5
Jamie Holdstock <jam.jar69@gmail.com>
2017-09-10 12:49:19 +0100

    finish charade section

--- android/CrosswordDictionary/app/src/main/java/com/jamieholdstock/crossword/activities/tutorial/devices/CharadeActivity.java
+++ android/CrosswordDictionary/app/src/main/java/com/jamieholdstock/crossword/activities/tutorial/devices/CharadeActivity.java
@@ -17,9 +17,10 @@ public class CharadeActivity extends FragmentSwiperActivity {
 
         final ArrayList<SolvedClue> clues = new ArrayList<SolvedClue>(){{
             add(new SolvedClue("[Little river]'s [carbon] [stink] (5)", "CREEK (C + REEK)"));
-            add(new SolvedClue("", ""));
-            add(new SolvedClue("", ""));
-            add(new SolvedClue("", ""));
+            add(new SolvedClue("[Regarding] the [Spanish] film (4)", "REEL (RE + EL)"));
+            add(new SolvedClue("Pizza topping with [capsicum] [on] half of [i]t (9)", " PEPPERONI (PEPPER + ON + I"));
+            add(new SolvedClue("[Friend] follows [child] completely (7)", "TOTALLY (TOT + ALLY)"));
+            add(new SolvedClue("[Damage] [church] with procession (5)", "MARCH (MAR + CH)"));
         }};
 
         return new ArrayList<Fragment>() {{
@@ -27,6 +28,7 @@ public class CharadeActivity extends FragmentSwiperActivity {
             add(new TutorialFragment(R.layout.devices_frag_char_roman_num));
             add(new TutorialFragment(R.layout.devices_frag_char_phonetic));
             add(new TutorialFragment(R.layout.devices_frag_char_science));
+            add(new TutorialFragment(R.layout.devices_frag_char_advanced));
             add(new ClueListFragment(clues, "Here are some examples of charade clues:"));
         }};
     }
