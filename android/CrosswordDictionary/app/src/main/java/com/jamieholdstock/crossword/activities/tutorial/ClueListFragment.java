b32579806be61954e1f60173be464fcdf90d97c3
Jamie Holdstock <jam.jar69@gmail.com>
2017-09-17 10:54:40 +0100

    Add listing screenshots.

--- android/CrosswordDictionary/app/src/main/java/com/jamieholdstock/crossword/activities/tutorial/ClueListFragment.java
+++ android/CrosswordDictionary/app/src/main/java/com/jamieholdstock/crossword/activities/tutorial/ClueListFragment.java
@@ -1,7 +1,6 @@
 package com.jamieholdstock.crossword.activities.tutorial;
 
 import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
